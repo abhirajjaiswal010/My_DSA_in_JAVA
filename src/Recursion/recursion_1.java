38de9dedf679941febfdb81d917cc90f6bb10761
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-08-23 20:26:58 +0530

    init commit

--- src/Recursion/recursion_1.java
+++ src/Recursion/recursion_1.java
@@ -1,5 +1,19 @@
+// find nth Fibonacci No.
 public class recursion_1 {
+    //function declare
+    static int fibo(int n)
+    {
+        if (n == 0) return 0;   // base case 1
+        if (n == 1) return 1;   // base case 2
+        return fibo(n - 1) + fibo(n - 2);  // recursive relation
+    }
     public static void main(String[] args) {
 
+        int n = 10; // print first 10 Fibonacci numbers
+
+        System.out.println("Fibonacci Series (using recursion):");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibo(i) + " ");
+        }
     }
 }
